b69ed4b3b22467d6eaae1839fe03d97d8ff0b53b
bazyl <svintcov95@gmail.com>
2017-02-26 21:13:23 +0200

    Main class were refactored

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -3,14 +3,7 @@ package lab1;
 public class Main {
 
     public static void main(String[] args) {
-
-
-        Model model = new Model();
-        View view = new View();
-        Controller controller = new Controller(model, view);
-        // Run
+        Controller controller = new Controller(new Model(), new View());
         controller.processUser();
-
-
     }
 }
